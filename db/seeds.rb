220cb7b3327377685d05ccd5e10a327a7892524a
hari@orangebicycle.co <hari@orangebicycle.co>
2019-07-19 17:18:54 +0530

    Added Admin data and a default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: "Admin", email: "admin@5thcolumn.net", password: "admin123", password_confirmation: "admin123", is_admin: true)
+user.confirm
+
+User.create(name: "cmullen", email: "cmullen@test.com", password: "test123", password_confirmation: "test123").confirm
